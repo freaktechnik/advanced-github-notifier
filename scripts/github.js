9db2577ac079c18ba1f1fb3b55798c730bf2965c
Martin Giger <martin@humanoids.be>
2025-05-17 05:41:45 +0200

    feat: reuse github headers for token validation

--- scripts/github.js
+++ scripts/github.js
@@ -155,9 +155,8 @@ export default class GitHub {
                 'access_token': token,
             }),
             headers: {
+                ...this.headers,
                 Authorization: `Basic ${globalThis.btoa(`${this.clientID}:${this.clientSecret}`)}`,
-                Accept: 'application/vnd.github+json',
-                'X-GitHub-Api-Version': '2022-11-28',
             },
         });
         if(method == "POST") {
