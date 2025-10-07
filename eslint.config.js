b6926689c1efa7a443b6f9bbfd3d6ffaa3bdee5f
Martin Giger <martin@humanoids.be>
2025-10-07 09:46:21 +0200

    chore: don't use eslint config node for this.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
 import freaktechnikConfigExtension from "@freaktechnik/eslint-config-extension";
 import freaktechnikConfigTest from "@freaktechnik/eslint-config-test";
-import freaktechnikConfigNode from "@freaktechnik/eslint-config-node";
 
 const SECOND_ITEM = 1;
 
 export default [
     ...freaktechnikConfigExtension,
     ...freaktechnikConfigTest,
-    ...freaktechnikConfigNode.slice(SECOND_ITEM),
     {
         files: [ "scripts/**/*.js" ],
         rules: {
